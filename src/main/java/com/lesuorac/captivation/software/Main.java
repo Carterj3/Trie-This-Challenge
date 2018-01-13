702ff0c5cdf280b7d568a44843e48be63d3c7703
Jeffrey.Carter <jeffrey.carter@JeffreyrtersMBP.fios-router.home>
2018-01-12 19:29:57 -0500

    created project

---
+++ src/main/java/com/lesuorac/captivation/software/Main.java
@@ -0,0 +1,10 @@
+package com.lesuorac.captivation.software;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/lesuorac/captivation/software/trie/TrieTest.java
@@ -0,0 +1,5 @@
+package com.lesuorac.captivation.software.trie;
+
+public class TrieTest {
+
+}
